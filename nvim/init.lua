d31838f573566cf9bcb3b307b99b859667cf221e
Luke Hollinda <lhollinda@demonware.net>
2024-05-05 22:27:09 -0700

    feat(nvim):indent blank lines

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,7 +103,7 @@ require('lazy').setup({
     },
   },
 
-    {
+  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
@@ -116,6 +116,18 @@ require('lazy').setup({
       },
     },
   },
+
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    opts = {
+      char = '┊',
+      show_trailing_blankline_indent = false,
+    },
+  },
+
 
 }, {})
 
