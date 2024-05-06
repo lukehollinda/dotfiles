9116c513de47993f74df0680f5af5a38cd02554f
Luke Hollinda <lhollinda@demonware.net>
2024-05-06 10:29:05 -0700

    feat(nvim): additional vim maps

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -8,3 +8,13 @@ local map = vim.api.nvim_set_keymap
 -- leader-h unhighlight search
 map('n', '<leader>h', ':noh<CR>', {noremap = true, silent = false})
 
+-- snap left and right
+map('n', 'H', '^', {noremap = true, silent = false})
+map('n', 'L', '$', {noremap = true, silent = false})
+
+-- clear file whitespace
+map("n", "<leader>cw", [[ :%s/\s\+$//g<CR>]], { noremap = true, silent = false})
+
+-- Mystical line to provide relative line numbers in netrw
+vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+
