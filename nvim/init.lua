3f5915a2cd611727474cd67c22379c12fb4603a3
Luke Hollinda <lhollinda@demonware.net>
2024-05-12 19:59:49 -0700

    feat(nvim): update to latest indent-blankline.nvim
    Was greated by error asking to upgrade to v3

--- nvim/init.lua
+++ nvim/init.lua
@@ -120,9 +120,8 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
     },
+    main = "ibl",
   },
 
     -- "gc" to comment visual regions/lines
