dd6c395376bc48f56eed6b308b72b64043c3760f
Luke Hollinda <luke.hollinda@gmail.com>
2025-03-21 22:20:27 -0700

    feat(nvim): add plugin for highlighting indentation

---
+++ nvim/lua/plugins/indent.lua
@@ -0,0 +1,9 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+
+    -- Config options can be viewed at :help ibl.config
+}
