30ceabbac2deb2aac2a49a4fa80173e12c6ab672
Luke Hollinda <luke.hollinda@gmail.com>
2025-03-21 22:20:26 -0700

    feat(nvim/sessions): quality of life options

--- nvim/lua/plugins/sessions.lua
+++ nvim/lua/plugins/sessions.lua
@@ -9,7 +9,11 @@ return {
     opts = {
       suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
       -- log_level = 'debug',
-    }
+      auto_create = function()
+        local cmd = 'git rev-parse --is-inside-work-tree'
+        return vim.fn.system(cmd) == 'true\n'
+      end,
+    },
   }
 
 }
