09cb848759d1b3dd836d44379923dbbf9ce726ac
Luke Hollinda <luke.hollinda@gmail.com>
2025-03-21 22:20:23 -0700

    feat(nvim): status line

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,6 +103,20 @@ require('lazy').setup({
     },
   },
 
+    {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        --theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
 }, {})
 
 -- [[ Configure LSP ]]
