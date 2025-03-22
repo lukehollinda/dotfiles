2502013f4cfe4e6e6446cfeebe49a0756753a649
Luke Hollinda <luke.hollinda@gmail.com>
2025-03-21 22:20:27 -0700

    feat(nvim): init lualine

---
+++ nvim/lua/plugins/lauline.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function (_, opts)
+    require('lualine').setup {
+      -- Default config here: https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#default-configuration
+      options = {
+        theme = "ayu_dark",
+        lualine_z = {
+          -- Custom location. line number : total lines
+          function ()
+            local line = vim.fn.line('.')
+            local last = vim.fn.line('$')
+            return string.format('%3d:%-2d', line, last)
+          end
+        }
+      }
+    }
+  end
+}
