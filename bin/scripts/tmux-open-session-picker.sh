b0c2851bca8c16f9d5c658d944ad74c93e71cd1f
Luke Hollinda <luke.hollinda@gmail.com>
2025-04-05 01:33:43 -0700

    feat(tmux): open session picker

---
+++ bin/scripts/tmux-open-session-picker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+selected=$(tmux list-sessions | awk '{print $1}' | sed 's/://' | fzf)
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    echo Must run in tmux
+    exit 0
+fi
+
+tmux switch-client -t "$selected"
+
