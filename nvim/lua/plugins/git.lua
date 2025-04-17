85b22b05008e075a244724bee5c8ce187deecaa2
Luke Hollinda <luke.hollinda@gmail.com>
2025-04-17 14:54:21 -0700

    feat(nvim): enable current line blame

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -12,6 +12,10 @@ return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+              delay = 400,
+      },
       signs = {
         add = { text = '+' },
         change = { text = '~' },
