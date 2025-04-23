b987dca5239b03e4bea9faf78ad69b5517e90ee9
Luke Hollinda <luke.hollinda@gmail.com>
2025-04-23 12:11:19 -0700

    chore(nvim): cleanup remaps

--- nvim/lua/misc-bindings.lua
+++ nvim/lua/misc-bindings.lua
@@ -1,9 +1,22 @@
+local map = vim.keymap.set
+
 -- Keymaps for better default experience
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+map({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gkzz' : 'kzz'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gjzz' : 'jzz'", { expr = true, silent = true })
+map('n', 'k', "v:count == 0 ? 'gkzz' : 'kzz'", { expr = true, silent = true })
+map('n', 'j', "v:count == 0 ? 'gjzz' : 'jzz'", { expr = true, silent = true })
 
 -- Unhighlight search term
-vim.keymap.set('n', '<ESC>', ':noh<CR>')
+map('n', '<ESC>', ':noh<CR>')
+
+-- resize windows
+map("n", "<Left>", ":vertical resize +1<CR>")
+map("n", "<Right>", ":vertical resize -1<CR>")
+map("n", "<Down>", ":resize +1<CR>")
+map("n", "<Up>", ":resize -1<CR>")
+
+map("n", "<c-w>H", ":vertical resize +1<CR>")
+map("n", "<c-w>L", ":vertical resize -1<CR>")
+map("n", "<c-w>K", ":resize +1<CR>")
+map("n", "<Up>J", ":resize -1<CR>")
