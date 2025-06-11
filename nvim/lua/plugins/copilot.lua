fe0211da622d50521a340e074f004b8c4c4e7e26
Luke Hollinda <luke.hollinda@gmail.com>
2025-06-11 16:25:56 -0700

    feat(nvim): enable copilot globally

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,12 +2,12 @@ return {
   {
     'github/copilot.vim',
     init = function()
-      -- disable by default
-      vim.g.copilot_filetypes = {
-        ["*"] = false,
-      }
-      -- explicitly request for copilot suggestions on Ctrl-C
-      vim.keymap.set('i', '<C-C>', '<Plug>(copilot-suggest)')
+      -- -- disable by default
+      -- vim.g.copilot_filetypes = {
+      --   ["*"] = false,
+      -- }
+      -- -- explicitly request for copilot suggestions on Ctrl-C
+      -- vim.keymap.set('i', '<C-C>', '<Plug>(copilot-suggest)')
     end,
   },
   {
