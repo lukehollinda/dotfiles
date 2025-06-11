a498c8faf6ba703cfca7c71e736a7f9f7fd920d7
Luke Hollinda <luke.hollinda@gmail.com>
2025-06-11 13:47:35 -0700

    feat(copilot): add CopilotChat.nvim plugin integration
    Shnazzy AI generated commit message:
    Refactored copilot plugin configuration to use a table of plugin specs.
    Added CopilotChat.nvim with dependencies and build instructions for
    MacOS/Linux. Updated lazy-lock.json to include CopilotChat.nvim.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,13 +1,25 @@
 return {
-  'github/copilot.vim',
-
-  init = function()
-    -- disable by default
-    vim.g.copilot_filetypes = {
-            ["*"] = false,
-    }
-    -- explicitly request for copilot suggestions on Ctrl-C
-    vim.keymap.set('i', '<C-C>', '<Plug>(copilot-suggest)')
-  end,
-
+  {
+    'github/copilot.vim',
+    init = function()
+      -- disable by default
+      vim.g.copilot_filetypes = {
+        ["*"] = false,
+      }
+      -- explicitly request for copilot suggestions on Ctrl-C
+      vim.keymap.set('i', '<C-C>', '<Plug>(copilot-suggest)')
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
