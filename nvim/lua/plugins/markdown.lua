69194b167586312924c504b1449ea77a3a69b4e0
Luke Hollinda <luke.hollinda@gmail.com>
2025-07-24 11:49:36 -0700

    feat(nvim): install davidgranstrom/nvim-markdown-preview

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,8 +1,18 @@
 return {
+    {
+    --- Inline Markdown rendering
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you prefer nvim-web-devicons
 
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
+    },
+    {
+    --- Markdown Preview
+    'davidgranstrom/nvim-markdown-preview',
+    config = {},
+    }
+
+
 }
