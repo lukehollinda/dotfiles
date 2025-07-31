926bb22ec865fbd6e7207b1182fa1a54aaf861c9
Luke Hollinda <luke.hollinda@gmail.com>
2025-07-30 20:24:02 -0700

    fix(nvim): markdown code blocks

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,17 +1,28 @@
 return {
     {
-    --- Inline Markdown rendering
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you prefer nvim-web-devicons
+        --- Inline Markdown rendering
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you prefer nvim-web-devicons
 
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {
+            code = {
+                enabled = true,
+            },
+        },
+        init = function ()
+            -- Out of the blue was required to add this to fix code block rendering
+            vim.g.markdown_fenced_languages = {
+                "bash", "sh", "lua", "json", "yaml", "diff", "python", "go"
+            }
+
+        end,
     },
     {
-    --- Markdown Preview
-    'davidgranstrom/nvim-markdown-preview',
-    config = function ()
-    end,
+        --- Markdown Preview
+        'davidgranstrom/nvim-markdown-preview',
+        config = function ()
+        end,
     }
 }
