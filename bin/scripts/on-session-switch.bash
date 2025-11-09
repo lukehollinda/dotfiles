e36d2d1f2ac97626fea229f32d7523237f088460
Luke Hollinda <luke.hollinda@gmail.com>
2025-11-09 15:18:57 -0800

    feat(tmux): on-session-switch history hook

---
+++ bin/scripts/on-session-switch.bash
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+TMUX_SESSION_HISTORY="$HOME/.tmux/session_history"
+
+# Get current tmux session
+current_session=$(tmux display-message -p '#S')
+
+# Remove any duplicates from the session history
+sed -i '' "/^$current_session/d" "$TMUX_SESSION_HISTORY"
+
+# Add the current session to the top of the history
+echo "$current_session" | cat - "$TMUX_SESSION_HISTORY" > temp && mv temp "$TMUX_SESSION_HISTORY"
+
+# Limit the history to the last 10 sessions
+sed -I '11,$d' "$TMUX_SESSION_HISTORY"
+
+exit 0
