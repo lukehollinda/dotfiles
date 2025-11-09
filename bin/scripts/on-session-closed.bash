392745cab3db9eaa45c863c595e66ed653105268
Luke Hollinda <luke.hollinda@gmail.com>
2025-11-09 15:39:55 -0800

    feat(tmux): session-closed hook

---
+++ bin/scripts/on-session-closed.bash
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+
+# $1 = session closed name
+# It appears that we must pass the closed session name as an argument. Attempting
+# to display the session name here will return the session which is opened after
+# close.
+
+TMUX_SESSION_HISTORY="$HOME/.tmux/session_history"
+
+# Get current tmux session
+current_session="$1"
+
+date >> /tmp/tmux-log
+echo "Session closed: $current_session" >> /tmp/tmux-log
+
+# Remove from session history
+sed -i '' "/^$current_session/d" "$TMUX_SESSION_HISTORY"
+
+# Switch to previous session
+previous_session=$(head -n 1 "$TMUX_SESSION_HISTORY")
+tmux switch-client -t "$previous_session"
+
+exit 0
