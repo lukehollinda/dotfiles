c03c9406ab0b4caef931ea176b30435e8482d6a9
Luke Hollinda <luke.hollinda@gmail.com>
2025-12-16 10:27:12 -0800

    feat(nvim): git maps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,16 +1,19 @@
 return {
 
   {
+    -- Excellent Git plugin
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gb', ':G blame<CR>', { desc = '[G]it [B]lame' })
+      vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = '[G]it [S]tatus' })
+      vim.keymap.set('n', '<leader>gl', ':G log --oneline<CR>', { desc = '[G]it [L]log' })
+      vim.keymap.set('n', '<leader>gd', ':G diff<CR>', { desc = '[G]it [D]iff' })
     end
   },
   {
     'tpope/vim-rhubarb',
     -- Corperate Github URL defined elsewhere
   },
-
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -27,6 +30,4 @@ return {
       },
     },
   },
-
-
 }
