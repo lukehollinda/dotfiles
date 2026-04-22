4322d9688584568b78b40e723d8821fb0d1116de
Luke Hollinda <luke.hollinda@gmail.com>
2026-04-21 22:50:11 -0700

    chore(nvim): remove treewalker.nvim

--- nvim/lua/plugins/tree-sitter.lua
+++ nvim/lua/plugins/tree-sitter.lua
@@ -20,23 +20,6 @@ return {
       enable = true,
     },
   },
-  {
-    'aaronik/treewalker.nvim',
-    opts = {
-      -- Whether to briefly highlight the node after jumping to it
-      highlight = true,
-      -- How long should above highlight last (in ms)
-      highlight_duration = 250,
-      -- The color of the above highlight. Must be a valid vim highlight group.
-      -- (see :h highlight-group for options)
-      highlight_group = 'CursorLine',
-    },
-      -- movement
-      vim.keymap.set({ 'n', 'v' }, '<Up>', '<cmd>Treewalker Up<cr>', { silent = true }),
-      vim.keymap.set({ 'n', 'v' }, '<Down>', '<cmd>Treewalker Down<cr>', { silent = true }),
-      vim.keymap.set({ 'n', 'v' }, '<Left>', '<cmd>Treewalker Left<cr>', { silent = true }),
-      vim.keymap.set({ 'n', 'v' }, '<Right>', '<cmd>Treewalker Right<cr>', { silent = true }),
-  },
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
