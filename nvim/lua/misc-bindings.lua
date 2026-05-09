5c4fa4c6bd87c1f140b81e508244cb243cb8f1ba
Luke Hollinda <luke.hollinda@gmail.com>
2026-05-08 17:45:56 -0700

    feat(nvim): file path to clipboard

--- nvim/lua/misc-bindings.lua
+++ nvim/lua/misc-bindings.lua
@@ -7,3 +7,6 @@ map('n', '<ESC>', ':noh<CR>')
 
 -- Tabs (resize and other <C-w> bindings managed by hydra in plugins/hydra.lua)
 map("n", "<c-w>c", ":tabnew<CR>")
+
+-- Copy relative file path relative to clipboard
+map("n", "<leader>yy", ":!echo -n % | pbcopy<CR>")
